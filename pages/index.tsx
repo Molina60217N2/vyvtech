28a367b56ed2f8a0d388882c291def756578c0c4
Molina60217N2 <molinasalas16@gmail.com>
2024-01-30 13:55:05 -0600

    Products and featured products

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,14 @@ import { DrupalNode } from "next-drupal"
 import { drupal } from "lib/drupal"
 import { Layout } from "components/layout"
 import { NodeArticleTeaser } from "components/node--article--teaser"
+import { NodeProductTeaser } from "components/products/node--product--teaser"
+import FeaturedProducts from "components/products/featured--products"
 
 interface IndexPageProps {
-  nodes: DrupalNode[]
+  featured: DrupalNode[]
 }
 
-export default function IndexPage({ nodes }: IndexPageProps) {
+export default function IndexPage({ featured }: IndexPageProps) {
   return (
     <Layout>
       <Head>
@@ -21,17 +23,8 @@ export default function IndexPage({ nodes }: IndexPageProps) {
         />
       </Head>
       <div>
-        <h1 className="mb-10 text-6xl font-black">Latest Articles.</h1>
-        {nodes?.length ? (
-          nodes.map((node) => (
-            <div key={node.id}>
-              <NodeArticleTeaser node={node} />
-              <hr className="my-20" />
-            </div>
-          ))
-        ) : (
-          <p className="py-4">No nodes found</p>
-        )}
+        <FeaturedProducts
+        products = {featured}/>
       </div>
     </Layout>
   )
@@ -40,22 +33,22 @@ export default function IndexPage({ nodes }: IndexPageProps) {
 export async function getStaticProps(
   context
 ): Promise<GetStaticPropsResult<IndexPageProps>> {
-  const nodes = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
-    "node--article",
-    context,
+  const featured = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
+    "node--product", 
+    context, 
     {
-      params: {
-        "filter[status]": 1,
-        "fields[node--article]": "title,path,field_image,uid,created",
-        include: "field_image,uid",
-        sort: "-created",
-      },
+      params:{
+        "filter[field_featured_product]":1, //1 es true XD
+        "fields[node--product]":"title,path,field_product_image,uid,created,field_product_price",
+        include:"field_product_image,uid",
+        // "filter[status]":0,
+      }
     }
   )
 
   return {
     props: {
-      nodes,
+      featured
     },
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+const {nextui} = require("@nextui-org/react");
 module.exports = {
   mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
@@ -10,5 +14,6 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  darkMode: "class",
+  plugins: [require("@tailwindcss/typography"),nextui(),('flowbite/plugin'),],
 }
