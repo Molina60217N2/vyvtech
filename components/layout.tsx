fdaf7f08ee6867c77861fb0093599c179f1d2e30
KevZ <kevinjgo121@gmail.com>
2024-01-30 18:27:12 -0600

    The position of the footer fixed to stay on the bottom of the page

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,7 +11,7 @@ export function Layout({ children }) {
   return (
     <NextUIProvider>
       <PreviewAlert />
-      <div className="">
+      <div className="flex flex-col min-h-screen">
         <header>
           <div className="container flex items-center justify-between py-6 mx-auto">
             <Link href="/" className="text-2xl font-semibold no-underline">
@@ -27,7 +27,7 @@ export function Layout({ children }) {
             </Link>
           </div>
         </header>
-        <main className="container py-10 mx-auto">{children}</main>
+        <main className="container flex-1 py-10 mx-auto">{children}</main>
         <Footer/>
       </div>
     </NextUIProvider>
