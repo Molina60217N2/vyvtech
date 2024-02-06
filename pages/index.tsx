3767a484d400e5b7a8f5e58f5f2f42cbdddfbf4b
KevZ <kevinjgo121@gmail.com>
2024-02-06 17:02:01 -0600

    Added the newest products component to home page, also adjusted the button position on the products cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { GetStaticPropsResult } from "next";
-import { DrupalNode } from "next-drupal";
+import { DrupalNode, deserialize } from "next-drupal";
 
 import { drupal } from "lib/drupal"
 import { Layout } from "components/layout"
@@ -11,9 +11,10 @@ import Brands from "components/brands-static/brands"
 import Services from '@/components/homepage/Services'
 interface IndexPageProps {
   featured: DrupalNode[];
+  nodes: DrupalNode[];
 }
 
-export default function IndexPage({ featured }: IndexPageProps) {
+export default function IndexPage({ featured, nodes }: IndexPageProps) {
   return (
     <Layout>
       <Head>
@@ -30,6 +31,16 @@ export default function IndexPage({ featured }: IndexPageProps) {
       <div>
         <FeaturedProducts
         products = {featured}/>
+      </div>
+      <div className="bg-[#E4EAEE] pb-20">
+        <h2 className="text-4xl text-darkBlue border-[#0F5C9A] border-b-4 font-bold mb-14 xl:ml-[315px] pt-14 max-[767px]:text-center md:w-[445px] max-[1024px]:max-w-[300px] max-[767px]:mx-auto max-[1024px]:text-2xl max-[636px]:max-w-[150px] max-[636px]:pt-12 max-[636px]:mb-9 md:ml-[255px] min-[769px]:ml-[155px]">PRODUCTOS MÁS NUEVOS</h2>
+        <div className="grid min-[1279px]:grid-cols-4 grid-cols-2 justify-items-center max-[1365px]:mx-3 max-[1279px]:mx-20 gap-y-5 max-[800px]:mx-3 min-[1025px]:gap-5 max-[1024px]:gap-10 max-[600px]:gap-3">
+          {nodes.map((node) =>(
+            <div key={node.id}>
+              <NodeProductTeaser node={node} />
+            </div>
+          ))}
+        </div>
       </div>
     </Layout>
   );
@@ -52,9 +63,23 @@ export async function getStaticProps(
     }
   );
 
+  const nodes = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
+    "node--product",
+    context,
+    {
+      params: {
+        "fields[node--product]":
+          "title,path,field_product_image,uid,created,field_product_price,field_product_brand",
+        include: "field_product_image,uid,field_product_brand",
+        "page[limit]": 24,
+      },
+    }
+  );
+
   return {
     props: {
       featured,
+      nodes,
     },
   };
 }
