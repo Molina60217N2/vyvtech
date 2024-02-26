7b3c6a38a3bf67b9e3cddb72a6b6a07b26216bb1
Molina60217N2 <molinasalas16@gmail.com>
2024-02-26 11:57:02 -0600

    fixed featured products and validation of them

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,9 +50,7 @@ export default function IndexPage({
         <Brands />
       </div>
       <Services />
-      <div>
-        <FeaturedProducts products={featured} />
-      </div>
+      <div>{featured ? <FeaturedProducts products={featured} /> : ""}</div>
       <div className="bg-[#E4EAEE] pb-20">
         <h2 className="text-4xl text-darkBlue border-[#0F5C9A] border-b-4 font-bold mb-14 xl:ml-[315px] pt-14 max-[767px]:text-center md:w-[445px] max-[1024px]:max-w-[300px] max-[767px]:mx-auto max-[1024px]:text-2xl max-[636px]:max-w-[150px] max-[636px]:pt-12 max-[636px]:mb-9 md:ml-[255px] min-[769px]:ml-[155px]">
           PRODUCTOS MÁS NUEVOS
